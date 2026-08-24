6636a4657d581d9a6d7a226d6286df26439ab717
ViewmetricaMX <viewportmx360@gmail.com>
2026-08-23 23:51:41 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
    Formato: código de país + número, sin espacios ni símbolos.
    Ejemplo Torreón/Gómez Palacio/Lerdo (Coahuila/Durango, MX): "528711234567"
 -------------------------------------------------------------------------- */
-const WHATSAPP_NUMBER = "528714005421";
+const WHATSAPP_NUMBER = "AQUI_MI_NUMERO";
 const WHATSAPP_MESSAGE = "Hola, vi la demo 360° del Nissan V-Drive y me gustaría conocer cómo podría aplicarse a nuestra agencia.";
 
 /* --------------------------------------------------------------------------
